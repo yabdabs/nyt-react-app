e8045114ea13530523e7513017a6da713e70bb14
Abdul Khan <abdul.k.khan75@gmail.com>
2017-08-05 00:21:25 -0400

    push to herkou

--- nyt-react/server.js
+++ nyt-react/server.js
@@ -31,8 +31,25 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
+//for heroku react in place of static public route above
+app.use(express.static("build"));
+
+// // ==== if its production environment!
+// if (process.env.NODE_ENV === 'production') {
+// 	const path = require('path')
+// 	console.log('YOU ARE IN THE PRODUCTION ENV')
+// 	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+// 	app.get('/', (req, res) => {
+// 		res.sendFile(path.join(__dirname, '../build/'))
+// 	})
+// }
+
+
+app.get("/", function(req, res){ 
+	res.sendFile(__dirname + "/build/static/index.html");
+});
 // -------------------------------------------------
 
 // MongoDB configuration (Change this URL to your own DB)
